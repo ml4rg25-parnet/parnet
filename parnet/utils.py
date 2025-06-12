a5fc970d92c7252059003d5ae1a56bc3200ca3ad
Lambert Moyon <lambosaur@users.noreply.github.com>
2025-06-12 09:25:17 +0200

    Update utils.py

--- parnet/utils.py
+++ parnet/utils.py
@@ -70,3 +70,48 @@ def sample_to_torch_sparse_tensor_dict(example):
         'inputs': tf.nest.map_structure(lambda x: to_sparse_tensor_dict(torch.tensor(x.numpy())), example['inputs']),
         'outputs': tf.nest.map_structure(lambda x: to_sparse_tensor_dict(torch.tensor(x.numpy())), example['outputs']),
     }
+
+def load_parnet_model_for_prediction(
+    model_weigth_path: os.PathLike,
+    device: torch.device,
+    dtype: torch.dtype,
+    is_old_model: bool = False,
+) -> parnet.models.RBPNet:
+    """Load a parnet model for prediction."""
+    model = torch.load(
+        model_weigth_path,
+        map_location=device,
+        weights_only=False,
+    ).to(dtype)
+
+    # disable dropout for deterministic results
+    model.eval()
+
+    # Parnet is still under heavy dev. and some "older" models are lacking
+    # components in their architectures. For prediction it is enough to
+    # simply set these components to None.
+    if is_old_model:
+        model.projection = None
+        model.head.control_nograd = False
+
+
+    return model
+
+
+def print_basic_info_parnet_model(
+    model: parnet.models.RBPNet,
+    device: torch.device,
+    dtype: torch.dtype,
+) -> None:
+    """Display basic information about the model."""
+    # Display number of trainable parameters
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+    print(f"Number of trainable parameters: {params:_}")
+
+    # Display available keys in output from output heads.
+    tmp_seq = torch.stack([parnet.utils.sequence_to_onehot("A").float()]).to(
+        device=device, dtype=dtype
+    )
+    print("Available output data: ", model({"sequence": tmp_seq}).keys())
+
